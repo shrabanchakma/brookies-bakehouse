4101dddf7ac64dda69e30414bdfe80451966bc6e
Shraban Chakma <shrabanchakma328@gmail.com>
2025-02-03 18:24:03 +0600

    feat: filter & pagination added in cookies page

--- src/Components/Pages/Cookies/CookiesFilter.jsx
+++ src/Components/Pages/Cookies/CookiesFilter.jsx
@@ -1,46 +1,62 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 import { FiDollarSign } from "react-icons/fi";
 
-const CookiesFilter = (props) => {
-  const [minPrice, setMinPrice] = useState(0);
-  const [maxPrice, setMaxPrice] = useState(100);
+const CookiesFilter = ({ filters, setFilters }) => {
   return (
-    <section className="bg-pink-100 h-full px-5 py-10 ">
-      {" "}
-      <div className="mb-6 text-brookies-primary ">
+    <section className="h-full px-5 py-10">
+      <div className="mb-6 text-brookies-primary">
         <h3 className="mb-2 text-[14px]">Availability</h3>
         <div className="border border-b-0 border-brookies-primary mb-5"></div>
         <div className="space-y-2">
           <label className="flex items-center">
             <input
               type="checkbox"
-              className="checkbox checkbox-xs  rounded-none"
+              checked={filters.inStock}
+              onChange={() =>
+                setFilters((prev) => ({
+                  ...prev,
+                  inStock: !prev.inStock,
+                }))
+              }
+              className="checkbox checkbox-xs rounded-none"
             />
-            <span className="ml-2">In stock (18)</span>
+            <span className="ml-2">In stock</span>
           </label>
           <label className="flex items-center">
             <input
               type="checkbox"
-              className="checkbox checkbox-xs  rounded-none"
+              checked={filters.outOfStock}
+              onChange={() =>
+                setFilters((prev) => ({
+                  ...prev,
+                  outOfStock: !prev.outOfStock,
+                }))
+              }
+              className="checkbox checkbox-xs rounded-none"
             />
-            <span className="ml-2">Out of stock (6)</span>
+            <span className="ml-2">Out of stock</span>
           </label>
         </div>
       </div>
-      {/* Price Range Section */}
+
       <div className="text-brookies-primary">
         <h3 className="mb-2 text-[14px]">Price</h3>
-        <div className="border border-b-0 border-brookies-primary mb-5 "></div>
+        <div className="border border-b-0 border-b-brookies-primary mb-5"></div>
         <div className="flex items-center space-x-4">
           <div className="flex items-center">
             <FiDollarSign />
             <input
               type="number"
-              value={minPrice}
+              min="0"
+              max={filters.maxPrice}
+              onChange={(e) =>
+                setFilters((prev) => ({
+                  ...prev,
+                  minPrice: Number(e.target.value),
+                }))
+              }
+              className="p-1 py-[0.3rem] rounded-md border border-brookies-primary w-20 focus:outline-2 focus:outline-brookies-primary text-[14px]"
               placeholder="From"
-              onChange={(e) => setMinPrice(e.target.value)}
-              className="p-1 py-[0.3rem] rounded-md border border-brookies-primary w-20  focus:outline-2 focus:outline-brookies-primary text-[14px]"
             />
           </div>
           <span>-</span>
@@ -48,9 +64,14 @@ const CookiesFilter = (props) => {
             <FiDollarSign />
             <input
               type="number"
-              value={maxPrice}
-              onChange={(e) => setMaxPrice(e.target.value)}
-              className="p-1 py-[0.3rem] rounded-md border border-brookies-primary w-20  focus:outline-2 focus:outline-brookies-primary text-[14px]"
+              min={filters.minPrice}
+              onChange={(e) =>
+                setFilters((prev) => ({
+                  ...prev,
+                  maxPrice: Number(e.target.value),
+                }))
+              }
+              className="p-1 py-[0.3rem] rounded-md border border-brookies-primary w-20 focus:outline-2 focus:outline-brookies-primary text-[14px]"
               placeholder="To"
             />
           </div>
@@ -59,7 +80,5 @@ const CookiesFilter = (props) => {
     </section>
   );
 };
-
-CookiesFilter.propTypes = {};
 
 export default CookiesFilter;
--- src/Components/Pages/Cookies/CookiesPage.jsx
+++ src/Components/Pages/Cookies/CookiesPage.jsx
@@ -1,15 +1,52 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState } from "react";
 import { useLoaderData, useNavigation } from "react-router-dom";
 import Container from "../../Shared/Container";
 import CookiesFilter from "./CookiesFilter";
+import Cookie from "./Cookie";
+import { LuArrowLeft, LuArrowRight } from "react-icons/lu";
 
-const CookiesPage = (props) => {
+const CookiesPage = () => {
   const cookies = useLoaderData();
   const navigation = useNavigation();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(6);
+  const [filters, setFilters] = useState({
+    minPrice: 0,
+    maxPrice: null,
+    inStock: false,
+    outOfStock: false,
+  });
+
   if (navigation.state === "loading") {
     return <div className="text-center my-8">Loading...</div>;
   }
+
+  const filteredCookies = cookies.filter((cookie) => {
+    const isMinSet = filters.minPrice !== null;
+    const isMaxSet = filters.maxPrice !== null;
+
+    const priceMatch =
+      (!isMinSet || cookie.price >= filters.minPrice) &&
+      (!isMaxSet || cookie.price <= filters.maxPrice);
+
+    const stockMatch =
+      (filters.inStock && cookie.inStock) ||
+      (filters.outOfStock && !cookie.inStock) ||
+      (!filters.inStock && !filters.outOfStock);
+    return priceMatch && stockMatch;
+  });
+
+  const totalPages = Math.ceil(filteredCookies.length / itemsPerPage);
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentCookies = filteredCookies.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   return (
     <Container>
@@ -23,16 +60,62 @@ const CookiesPage = (props) => {
           ship.
         </p>
       </div>
-      <div className="flex items-start w-full  h-[90rem] bg-green-500">
-        <div className="w-1/6  h-[50rem] sticky top-0">
-          <CookiesFilter />
+
+      <div className="flex items-start w-full">
+        <div className="w-1/6 h-[25rem] sticky top-0">
+          <CookiesFilter filters={filters} setFilters={setFilters} />
         </div>
-        <div className="w-5/6 bg-orange-100 min-h-screen">cookies</div>
+
+        <div className="w-5/6 min-h-screen mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 lg:gap-2 xl:gap-0 mt-16">
+            {currentCookies.length > 0 ? (
+              currentCookies.map((cookie) => (
+                <Cookie key={cookie.id} cookie={cookie} />
+              ))
+            ) : (
+              <p className="text-center text-brookies-primary mt-4">
+                No cookies found!
+              </p>
+            )}
+          </div>
+
+          <div className="flex justify-between mt-8">
+            <button
+              onClick={() => handlePageChange(currentPage - 1)}
+              className={`flex justify-end items-center mx-1 ${
+                currentPage === 1 && "invisible"
+              }`}
+            >
+              <LuArrowLeft size={25} />
+            </button>
+            <div className="flex items-center gap-2">
+              {Array.from({ length: totalPages }, (_, index) => (
+                <button
+                  key={index + 1}
+                  onClick={() => handlePageChange(index + 1)}
+                  className={`text-brookies-primary mx-1 ${
+                    currentPage === index + 1
+                      ? "border-b-[1px] border-b-brookies-primary px-2"
+                      : "px-2"
+                  }`}
+                >
+                  {index + 1}
+                </button>
+              ))}
+            </div>
+            <button
+              onClick={() => handlePageChange(currentPage + 1)}
+              className={`flex justify-end items-center mx-1  ${
+                currentPage === totalPages && "invisible"
+              }`}
+            >
+              <LuArrowRight size={25} />
+            </button>
+          </div>
+        </div>
       </div>
     </Container>
   );
 };
-
-CookiesPage.propTypes = {};
 
 export default CookiesPage;
